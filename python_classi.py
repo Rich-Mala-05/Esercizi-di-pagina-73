82cc358263d35ca6711b605aa1f919fe4ebe8aa1
Malla <Rich-Mala-05>
2021-03-17 17:34:41 +0100

    Aggiunto esercizio del campo di mostri sulle classi

---
+++ python_classi.py
@@ -0,0 +1,92 @@
+class Entity: 
+  def __init__(self, x, y, field):
+    self.x = x
+    self.y = y
+    self.field = field
+    self.field.entities.append(self)
+
+  def move(self, direction):
+    can_move = True
+    if direction == "up" and self.y >= 1:
+      for e in field.entities:
+        if self.x == e.x and self.y-1 == e.y:
+          can_move = False
+          print("Casella occupata")      
+      if can_move:
+        self.y -= 1
+    
+    elif direction == "down" and self.y <= field.h-2:
+      for e in field.entities:
+        if self.x == e.x and self.y+1 == e.y:
+          can_move = False      
+          print("Casella occupata")
+      if can_move:
+        self.y += 1
+    
+    elif direction == "left" and self.x >= 1:
+      for e in field.entities:
+        if self.x-1 == e.x and self.y == e.y:
+          can_move = False
+          print("Casella occupata")      
+      if can_move:
+        self.x -= 1
+    
+    elif direction == "right" and self.x <= field.w-2:
+      for e in field.entities:
+        if self.x+1 == e.x and self.y == e.y:
+          can_move = False
+          print("Casella occupata")      
+      if can_move:
+        self.x += 1
+    
+    else:
+      print("Hai raggiunto il limite del campo")
+
+class Monster(Entity):
+  def __init__(self, x, y, name, damage, field):
+    super().__init__(x, y, field)
+    self.name = name
+    self.hp = 10
+    self.damage = damage
+
+  def info(self):
+    print("sono", self.name, "hp:", self.hp, "/10", "e mi trovo a", self.x, ",", self.y)
+
+  def attack(self, enemy):
+    if self.hp <= 0:
+      print(self.name, "prova ad attaccare da morto con scarsi risultati")
+    else: 
+      print(self.name, "attacca", enemy.name)
+
+      if (enemy.hp <= 0):
+        print(enemy.name, "e' morto")
+      else:
+        enemy.hp -= self.damage
+
+class Field:
+  def __init__(self):
+    self.w = 5
+    self.h = 5
+    self.entities = []
+  
+  def draw(self):
+    for y in range(self.h):
+      for x in range(self.w):
+        for e in self.entities:
+          if x == e.x and y == e.y:
+            print("[x]", end = "")
+            break    
+        else:
+          print("[ ]", end = "")
+      print()
+
+field = Field()
+m = Monster(2, 2, "Pino", 10, field)
+m = Monster(1, 0, "Caio", 10, field)
+m = Monster(1, 1, "Gino", 10, field)
+
+
+for i in range(1000):
+  field.draw()
+  m.move(input())
+  print()
